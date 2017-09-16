a194312cd8a8b360a85a4956bb5026138c0ef92c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-09-16 18:00:52 +0900

    extact run method

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"time"
@@ -80,39 +81,39 @@ func rest(duration time.Duration) {
 	scanner.Scan()
 }
 
-func main() {
-	const version = "0.1.0"
+func run(args []string, outStream, errStream io.Writer) int {
 	var show string
 
 	flags := flag.NewFlagSet("goma", flag.ExitOnError)
+	flags.SetOutput(errStream)
 	flags.StringVar(&show, "s", "", "Show your tomatoes. You can specify range, 'today', 'week', 'month' or 'all'.")
-	flags.Parse(os.Args[1:])
+	flags.Parse(args[1:])
 
 	err := initDB()
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
+		return 1
 	}
 
 	if len(show) != 0 {
 		if !contains([]string{"today", "week", "month", "all"}, show) {
 			fmt.Printf("'%s' is invalid argument. Please specify 'today', 'week', 'month' or 'all'.\n", show)
-			os.Exit(1)
+			return 1
 		}
 
 		err = showTomatoes(show)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
-			os.Exit(1)
+			return 1
 		}
-		os.Exit(0)
+		return 0
 	}
 
 	for i := 1; ; i++ {
 		err = task()
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
-			os.Exit(1)
+			return 1
 		}
 
 		if i%4 == 0 {
@@ -121,4 +122,8 @@ func main() {
 			rest(restDuration)
 		}
 	}
+}
+
+func main() {
+	os.Exit(run(os.Args, os.Stdout, os.Stderr))
 }
